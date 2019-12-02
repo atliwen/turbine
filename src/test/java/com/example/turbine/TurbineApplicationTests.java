fd3b5bc4648a12b4f1ba9917487486f3efad49f4
李文 <lw258083@zjs.com.cn>
2019-12-02 11:36:23 +0800

    turbine

--- src/test/java/com/example/turbine/TurbineApplicationTests.java
+++ src/test/java/com/example/turbine/TurbineApplicationTests.java
@@ -1,17 +1,29 @@
 //package com.example.turbine;
 //
 //import org.junit.Test;
-//import org.junit.runner.RunWith;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.test.context.junit4.SpringRunner;
 //
-//@RunWith(SpringRunner.class)
-//@SpringBootTest
+//import java.math.BigDecimal;
+//
+////
+////import org.junit.Test;
+////import org.junit.runner.RunWith;
+////import org.springframework.boot.test.context.SpringBootTest;
+////import org.springframework.test.context.junit4.SpringRunner;
+////
+////@RunWith(SpringRunner.class)
+////@SpringBootTest
 //public class TurbineApplicationTests
 //{
 //
+//
+//
+//
 //    @Test
 //    public void contextLoads() {
+//
+//        BigDecimal  b=new BigDecimal("100.0");
+//        System.out.println(b.toString());
+//
 //    }
 //
 //}
